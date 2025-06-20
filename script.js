0ed9634213fbd3738bba7293b2dba49721c2939c
Shichang-Ye <yeshichangedu@gmail.com>
2025-06-20 11:48:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ window.addEventListener('DOMContentLoaded', () => {
     { angleDeg: 238, label: "Muskrat Quality" },
 
     { angleDeg: 262, label: "Ice", link: "https://www.arcgis.com/apps/dashboards/c6399a3e216047c9a46adb0e95d433c3" },
-    { angleDeg: 283.5, label: "Surface Water", link: "https://www.arcgis.com/apps/dashboards/51963a29c60740268b59a54a8b05cf46" },
+    { angleDeg: 283.5, label: "Surface Water", link: "https://www.arcgis.com/apps/dashboards/0c7c8218200f4970acdc744b23b48109" },
     { angleDeg: 305, label: "Lake Level", link: "https://www.arcgis.com/apps/dashboards/d8fa865c3ce44f76904eb8628ce9a288" },
     { angleDeg: 326.5, label: "Navigation", link: "https://www.arcgis.com/apps/dashboards/71317a760f0b4bf5a8962feb150a0547" },
     { angleDeg: 348, label: "River Flow", link: "https://www.arcgis.com/apps/dashboards/d9acccf8158c43a68b78527d0389ba13" },
