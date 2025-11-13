28d4ed416819d63decc715134cf3a04507926064
Shichang-Ye <yeshichangedu@gmail.com>
2025-11-13 10:21:49 -0800

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ window.addEventListener('DOMContentLoaded', () => {
     { angleDeg: 145.2, label: "Waterbird Egg Quality", link: "https://apca.maps.arcgis.com/apps/dashboards/4d5df48bbd494a67846d90fe8954b7cf" },
     { angleDeg: 168.4, label: "Snow Contamination", link: "https://apca.maps.arcgis.com/apps/dashboards/b2d78a7378a04318808c3c940e373dc3" },
     { angleDeg: 191.6, label: "Fish Quality",link: "https://apca.maps.arcgis.com/apps/dashboards/9122ce68c1c94218bf203bc45f3b05a1" },
-    { angleDeg: 214.8, label: "River Water Quality", link: "https://apca.maps.arcgis.com/apps/dashboards/54ab3e8180dd49a99a38abd37526f255" },
+    { angleDeg: 214.8, label: "River Water Quality", link: "https://www.arcgis.com/apps/dashboards/10be9736c9f84297b7f7b2db7963f0f5" },
     { angleDeg: 238, label: "Muskrat Quality" },
 
     { angleDeg: 262, label: "Ice", link: "https://www.arcgis.com/apps/dashboards/c6399a3e216047c9a46adb0e95d433c3" },
